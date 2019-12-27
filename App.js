d9cd4982eafc14128cd94ee4c633b134ea6914b2
cynthia sulaiman <cynthia.sulaiman@alfacart.com>
2019-12-27 15:35:24 +0700

    12/27/2019

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
-import Greeting from './app/TestComponent';
 import Form from './app/FormComponent';
 
 export default class HelloWorldApp extends Component {
   render() {
+    const wrapperStyle = {
+      flex: 1,
+      flexDirection: 'column',
+    };
+    const styles = {
+      headerStyle: {
+        backgroundColor: 'pink',
+        flex: 1,
+      },
+      bodyStyle: {
+        backgroundColor: 'white',
+        flex: 4,
+      },
+      footerStyle: {
+        backgroundColor: 'pink',
+        flex: 1,
+      },
+    }
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!</Text>
-        <Greeting name="test" />
-        <View style={{flex: 1, flexDirection: 'row'}}>
-          <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}}/>
+      // <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      //   <Text>Hello, world!</Text>
+      //   <Greeting name="test" />
+      //   <View style={{flex: 1, flexDirection: 'row'}}>
+      //     <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}}/>
+      //   </View>
+      //   <View>
+      //     <Form></Form>
+      //   </View>
+      //   <View>
+      //     <Button buttonText="clickhere"></Button>
+      //   </View>
+      // </View>
+      <View style={wrapperStyle}>
+        <View style={styles.headerStyle}>
+          <Text>This is the header.</Text>
         </View>
-        <View>
+        <View style={styles.bodyStyle}>
+          <Text>This is the body.</Text>
           <Form></Form>
+        </View>
+        <View style={styles.footerStyle}>
+          <Text>This is the footer.</Text>
         </View>
       </View>
     );
--- app/FormComponent.js
+++ app/FormComponent.js
@@ -1,12 +1,42 @@
 import React, {Component} from 'react';
-import {TextInput, Text, View} from 'react-native';
+import {TextInput, View, Button, Alert} from 'react-native';
+import axios from 'axios';
 
-class Form extends Component{
-  constructor(props){
+class Form extends Component {
+  constructor(props) {
     super(props);
     this.state ={
       text: ''
     }
+  }
+
+  postTask() {
+    axios({
+      method: 'post',
+      url: 'http://10.4.56.170:3000/tasks',
+      data: {
+        name: this.state.text,
+      },
+    })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+  getTasks(){
+    axios({
+      method: 'get',
+      url: 'http://10.4.56.170:3000/tasks',
+    })
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error){
+        console.log(error);
+      });
   }
 
   render() {
@@ -14,13 +44,12 @@ class Form extends Component{
       <View style={{padding: 10}}>
         <TextInput
           style={{height:40}}
-          placeholder="Type Here"
-          onChangeText={(text) => this.setState({text})}
+          placeholder="Type the task's name"
+          onChangeText={text => this.setState({text})}
           value={this.state.text}
         />
-        <Text style={{padding:10, fontSize:42}}>
-        current text = {this.state.text}
-        </Text>
+        <Button title="POST" onPress={() => this.postTask()} />
+        <Button title="GET TASKS" onPress={() => this.getTasks()} />
       </View>
     )
   }
